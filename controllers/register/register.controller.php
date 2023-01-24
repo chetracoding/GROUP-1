a659940adfcea69c51ad3cb3aeccf50766a55d15
ChetraGithub <hongchetra22@gmail.com>
2023-01-24 19:38:04 +0700

    CTA-20 Display register view

--- controllers/register/register.controller.php
+++ controllers/register/register.controller.php
@@ -1,20 +1,20 @@
 <?php
-// require 'models/register.model.php';
+require 'models/register.model.php';
+
 $errors = [];
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // $lastName = $_POST['lastName'];
-    // $email = $_POST['email'];
-    // $password = $_POST['password'];
-    // $passwordConfirm = $_POST['passwordConfirm'];
-    // $terms = $_POST['terms'];
-    // if (! isset($_POST['firstName'])) {
-    //     $errors['firstName'] = "Please enter";
-    // }
-    echo json_encode(['code'=>404, 'errors'=>$errors]);
-    
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    empty($_POST['first-name']) || trim($_POST['first-name']) == "" ? $errors['first-name'] = "Please enter first name." : "";
+    empty($_POST['last-name']) || trim($_POST['last-name']) == "" ? $errors['last-name'] = "Please enter first name." : "";
+
+    filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? "": $errors['email'] = "Please a valide email.";
+
+    strlen($_POST['password'] > 8) ? "" : $errors['password'] = "Please enter more than 8 characters.";
+    $_POST['password'] == $_POST['confirm-password'] ? "" : $errors['confirm-password'] = "Password not correct.";
+
+    isset($_POST['terms'])? "" : $errors['terms'] = "Please check terms.";
+
+
 }
-
-
 
 require "views/register/register.view.php";
