e944e4d66dc0111816f48c00167ed3927ff03a7b
SREYPHEAP.KHIN <sreypheap.khin@student.passerellesnumeriques.org>
2023-01-24 11:01:30 +0700

    [CTA-26] create form login view

---
+++ controllers/login/login.controller.php
@@ -0,0 +1,2 @@
+<?php
+require "views/login/login.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -57,7 +57,7 @@
         <!-- Profile dropdown -->
         <div class="relative ml-3">
           <div>
-          <a href="shows" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
+          <a href="/login" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" href=''>Login</a>
           </div>
 
           <!--
