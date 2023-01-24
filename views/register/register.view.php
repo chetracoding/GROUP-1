fa2a9846c239504c7220066193928ba1f447b06c
ChetraGithub <hongchetra22@gmail.com>
2023-01-24 20:51:41 +0700

    CTA-19 Update btn submit color

--- views/register/register.view.php
+++ views/register/register.view.php
@@ -42,7 +42,7 @@
                         <span class="text-red-600"><?= isset($errors['terms'])? $errors['terms'] : ""; ?></span>   
                     </div>
                 </div>
-                <button id="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-4 mb- dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-full">Register</button>
+                <button id="submit" type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-4 mb- dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 w-full">Register</button>
                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                     Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                 </p>
