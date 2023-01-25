34ca6c624f12c0c1430f615ed4c9c78d5692ce69
SREYPHEAP.KHIN <sreypheap.khin@student.passerellesnumeriques.org>
2023-01-25 14:57:22 +0700

    CTA-71 Display list shows by search

--- controllers/search/search.controller.php
+++ controllers/search/search.controller.php
@@ -1,3 +1,15 @@
 <?php
+require "models/search.model.php";
 
-require "views/search/search.view.php";
+$shows = [];
+$text = $_POST['search'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
+
+if (empty($shows)) {
+    require "views/search/search.view.php";
+
+} else {
+    require "views/errors/403.php";
+}
