70aa5be909fd57d26fd72f222bbfb9dd3aafca6d
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2023-01-25 14:02:54 +0700

    [CTA-65] Display details view

--- controllers/detail/detail.controller.php
+++ controllers/detail/detail.controller.php
@@ -1,1 +1,14 @@
+<?php
+require 'models/detail.model.php';
+$id = (int) $_GET['id'];
 
+if (is_int(intval($_GET['id']))) {
+
+  if (checkShow(intval($_GET['id']))) {
+      $details = getShowId($id );
+  } else {
+    header('Location: views/errors/404.php');
+  }
+
+}
+require "views/details/detail.view.php";
