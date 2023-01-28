238c1b9cb89d07ef4460d83ae06ef1e770bea250
ChetraGithub <hongchetra22@gmail.com>
2023-01-28 16:25:28 +0700

    CTA-58 Update list shows view

--- views/home/home.view.php
+++ views/home/home.view.php
@@ -1,49 +1,65 @@
 <?php require "views/partials/head.php" ?>
 <?php require "views/partials/nav.php" ?>
 <body>
-<video loop muted autoplay>
-        <source src="../../assets/videos/video.mp4" type="video/mp4">
-</video>
-<div id="container">
-    <div class="text">
-        <h1>INCEPTION</h1>
-        <div class="stars">
-            <i class="material-icons">star</i>
-            <i class="material-icons">star</i>
-            <i class="material-icons">star</i>
-            <i class="material-icons">star</i>
-            <i class="material-icons">star</i>
-        </div>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis dicta repellat voluptas.</p>
-    </div>
-</div>
-<div class="list">
-  <h1>List Shows</h1>
-</div>
-  <main >
+
+<main >
+
+  <div class="slider">
+		<div class="myslide fade">
+			<img src="https://primecineplex.com/Home/GetHeroBannerThumbnailPath?BannerID=264" style="width: 100%; height: 100%;">
+		</div>
+		
+		<div class="myslide fade">
+			<img src="https://primecineplex.com/Home/GetHeroBannerThumbnailPath?BannerID=233" style="width: 100%; height: 100%;">
+		</div>
+		
+		<div class="myslide fade">
+			<img src="https://primecineplex.com/Home/GetHeroBannerThumbnailPath?BannerID=278" style="width: 100%; height: 100%;">
+		</div>
+		
+		<div class="myslide fade">
+			<img src="https://primecineplex.com/Home/GetHeroBannerThumbnailPath?BannerID=279" style="width: 100%; height: 100%;">
+		</div>
+		
+		<div class="myslide fade">
+			<img src="https://primecineplex.com/Home/GetHeroBannerThumbnailPath?BannerID=280" style="width: 100%; height: 100%;">
+		</div>
+
+		<a class="prev hover:text-gray-800" onclick="plusSlides(-1)">&#10094;</a>
+  		<a class="next hover:text-gray-800" onclick="plusSlides(1)">&#10095;</a>
+		
+		<div class="dotsbox" style="text-align:center">
+			<span class="dot" onclick="currentSlide(1)"></span>
+			<span class="dot" onclick="currentSlide(2)"></span>
+			<span class="dot" onclick="currentSlide(3)"></span>
+			<span class="dot" onclick="currentSlide(4)"></span>
+			<span class="dot" onclick="currentSlide(5)"></span>
+		</div>
+
+	</div>
+
   <div class="grid grid-cols-4 gap-8 m-20">
       <?php foreach($shows as $show): ?>
-      <div id="card-shows" class=" max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-        <a href="#">
-            <img class="rounded-t-lg" src= <?php echo $show['image']?> alt="" />
-        </a>
-        <div class="p-5" id ="item-show">
-          <div>
-            <a href="#">
-                <h5 class="mb-2 text-2l font-bold  tracking-tight text-gray-900 text-white" ><?php echo $show['name']?></h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 text-gray-400 " ><?php echo $show['date']?></p>
+      <div id="card-shows" class="max-w-sm bg-gray-800 border border-gray-200 shadow dark:bg-gray-800 dark:border-gray-700 ">
+        <img class="" src= <?= $show['image']?> alt="" />
+
+        <div class="p-5 bg-gray-800" id ="item-show">
+          <div class="h-4/5 flex flex-col justify-between">
+            <h5 class="mb-2 text-2l font-bold  tracking-tight text-white" ><?= $show['name']?></h5>
+            <p class="mb-3 font-normal text-white" ><?= $show['date']?></p>
           </div>
           <div>
-              <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  Detail
-                  <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              <a href="/detail?id=<?= $show['show_id']?>" class="w-full inline-flex justify-center items-center px-3 py-2 text-sm font-normal  text-center text-white bg-green-600 rounded-lg hover:bg-green-800">
+                Detail
+                <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </a>
           </div>
         </div>
       </div>
     <?php endforeach; ?>
-  </div></main>
-</body>
+  </div>
 
+</main>
+
+<script src="views/home/js/script.js"></script>
 <?php require "views/partials/footer.php" ?>
