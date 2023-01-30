6680ff24d4ed0bd54503e3e6ea9b084f70bd7c63
ChetraGithub <hongchetra22@gmail.com>
2023-01-30 15:50:31 +0700

    Update input type date value

--- views/searchdate/searchdate.view.php
+++ views/searchdate/searchdate.view.php
@@ -6,7 +6,7 @@
     <label for="date" class="font-bold text-white">From date:</label>
     <input type="date"  name="start-date" id="start-date" value="<?= isset($_POST['start-date'])? $_POST['start-date'] : "" ?>" class="hover:bg-gray-100  border border-green-500 rounded-xl" >
     <label for="date" class="font-bold text-white">To date:</label>
-    <input type="date" name="end-date" id="end-date" value="<?= isset($_POST['end-date'])? $_POST['start-date'] : "" ?>" class="hover:bg-gray-100  border border-green-500 rounded-xl">
+    <input type="date" name="end-date" id="end-date" value="<?= isset($_POST['end-date'])? $_POST['end-date'] : "" ?>" class="hover:bg-gray-100  border border-green-500 rounded-xl">
     <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Search</button>
   </form>
 </div>
