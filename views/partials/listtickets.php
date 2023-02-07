4da96f5a7c000be127d5f1719f215a155a2a6877
THOEUN.SIENGDAV <THOEUN.SIENGDAV@PNC.LAN>
2023-02-07 16:34:37 +0700

    CTA-145 show information list of tickets

--- views/partials/listtickets.php
+++ views/partials/listtickets.php
@@ -1,36 +1,38 @@
+<?php foreach($tickets as $ticket): ?>
 
-<div class="bg-gray-800 from-blue-200 to-blue-100 border-b-4 border-blue-600 shadow-xl p-5">
-    <div class="text-slate-400 flex flex-row items-center">
-        <div class="flex-shrink flex flex-1 pr-2">
-            <img class="h-25 w-20"src="https://www.majorcineplex.com.kh/load_file/movie/file_20220926020910.jpg" alt="">
-        </div>
-        <div class="w-1/5 flex-2 flex flex-col">
-            <span>Title: </span>
-            <span class="font-bold"></span>
-        </div>
-        <div class="w-1/5 flex flex-col">
-            <span>Venue: </span>
-            <span class="font-bold"></span>
-        </div>
-        <div class="flex-1 flex flex-col">
-            <span>Release date: </span>
-            <span class="font-bold"></span>
-        </div>
-        <div class="flex-1 flex flex-col">
-            <span>Tickets: </span>
-            <span class="font-bold"></span>
-        </div>
-        <div class="flex-1 flex flex-col">
-            <span>Price: </span>
-            <span class="font-bold"></span>
-        </div>
-        <div>
-            <div class="p-3">
-                <a href="">
-                    <button class=" text-s flex-1 bg-green-500 text-black font-bold py-1 px-4 rounded">View</button>
-                </a>
+    <div class="bg-gray-800 from-blue-200 to-blue-100 border-b-4 border-white-600 shadow-xl p-5">
+        <div class="text-slate-400 flex flex-row items-center">
+            <div class="flex-shrink flex flex-1 pr-2">
+                <img class="h-25 w-20"src=<?= $ticket['image'] ?> alt="">
+            </div>
+            <div class="w-1/5 flex-2 flex flex-col">
+                <span>Title: </span>
+                <span class="font-bold"><?= $ticket['title'] ?></span>
+            </div>
+            <div class="w-1/5 flex flex-col">
+                <span>Venue: </span>
+                <span class="font-bold"><?= $ticket['name'] ?></span>
+            </div>
+            <div class="flex-1 flex flex-col">
+                <span>Release date: </span>
+                <span class="font-bold"><?= $ticket['date'] ?></span>
+            </div>
+            <div class="flex-1 flex flex-col">
+                <span>Tickets: </span>
+                <span class="font-bold"><?= $ticket['total_tickets'] ?></span>
+            </div>
+            <div class="flex-1 flex flex-col">
+                <span>Price: </span>
+                <span class="font-bold">$<?= $ticket['price'] * $ticket['total_tickets'] ?></span>
+            </div>
+            <div>
+                <div class="p-3">
+                    <a href="/printticket?=<?= $ticket['ticket_id']?>">
+                        <button class=" text-s flex-1 bg-green-500 text-black font-bold py-1 px-4 rounded">View</button>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
+<?php endforeach; ?>
