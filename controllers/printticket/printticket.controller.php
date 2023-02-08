7a902f615f4f7ccfb9d57c7023cf6d6f38a291b1
SREYPHEAP.KHIN <sreypheap.khin@student.passerellesnumeriques.org>
2023-02-08 18:53:07 +0700

    [CTA-132] Display print ticket controller

--- controllers/printticket/printticket.controller.php
+++ controllers/printticket/printticket.controller.php
@@ -1,3 +1,15 @@
 <?php require 'models/printticket.model.php';?>
-
+<?php
+    if (isset($_GET['id'])) {
+        $userId =  $_SESSION['user-id'];
+        $ticketTd = (int) $_GET['id'];
+        
+        if (checkTticketById($userId, $ticketTd)) {
+            $ticket = getTicketById($ticketTd);
+        } else {
+            require 'views/errors/404.php';
+            die();
+        }
+    }
+?>
 <?php require "views/printticket/printticket.view.php"?>;
