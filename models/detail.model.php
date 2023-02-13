93860307371adaaa7bf5cba441709a52ad420712
SOKI.CHHUOY <SOKI.CHHUOY@PNC.LAN>
2023-02-13 16:23:16 +0700

    [CTA-137] Update create booking view

--- models/detail.model.php
+++ models/detail.model.php
@@ -22,3 +22,11 @@ function getTimesById(int $id) : array
     $statement->execute([ ':id' => $id ]);
     return $statement->fetchAll();
 }
+
+function getVenue(int $id): array
+{
+    global $connection;
+    $statement = $connection->prepare("select name from venues inner join shows on shows.venue_id = venues.venue_id where show_id = :id;");
+    $statement->execute([ ':id' => $id ]);
+    return $statement->fetch();
+}
