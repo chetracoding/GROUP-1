1419bc94a2865cd422a01aa7780bb1bc9c01e646
THOEUN.SIENGDAV <THOEUN.SIENGDAV@PNC.LAN>
2023-02-17 17:25:17 +0700

    CTA-145 Update get list bookings from database

--- models/listtickets.model.php
+++ models/listtickets.model.php
@@ -1,10 +1,10 @@
 <?php
 
-function getTicketsByUserId(int $id) : array
+function getTicketsByUserId(int $userId) : array
 {
     global $connection;
-    $statement = $connection->prepare("select * from customer_tickets where user_id=:id;");
-    $statement->execute([':id' => $id]);
+    $statement = $connection->prepare("select ct.*, count(ct.seat_id) as 'total_seats' from customer_tickets ct where user_id = :user_id group by ticket_id;");
+    $statement->execute([':user_id' => $userId]);
     
     return $statement->fetchAll();
 }
